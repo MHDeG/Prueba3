2542bf4cb46aa1fd266fe131ff7addd4174a5a17
MarceloDG <mdegiglio@outlook.com>
2024-03-12 17:50:54 -0300

    Segundo commit

--- App.java
+++ App.java
@@ -2,7 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         int variable1 = 5;
         double variable2 = 2.03;
+
         System.out.println("Hello, World!");
+        
         System.out.println(variable1);
         System.out.println(variable2);
     }
